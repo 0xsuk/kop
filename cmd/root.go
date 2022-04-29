1d63224f3c7ccfe45c26815849149932768211cf
0xsuk <ikeuu53@yahoo.co.jp>
2022-04-29 20:04:09 +0900

    Help message update

--- cmd/root.go
+++ cmd/root.go
@@ -34,25 +34,6 @@ func Execute() {
 
 func init() {
 	configInit()
-	rootCmd.SetUsageFunc(func(cmd *cobra.Command) error {
-		fmt.Println(`Available Commands:
-                 help [<command>]       help about any command
-    [Variable]
-                 av <variable> <value>  add <variable> <value> pair
-                 cv <variable>          copy <variable>'s <value> to system clipboard
-                 rv <variable>...       remove variables. If no <variable> provided, remove all variables
-                 sv [<search_query>]    search variable that contains <search_query>. If no query provided, print all variables
-    [Command]
-                 ac                     add command
-                 cc <index>             copy command replacing <variable> with its <value>
-                 rc <index>...          remove commands specified by index
-                 sc [<search_query>]    search commands. If no query provided, print all commands
-                 
-Available Flags:
-                 kop [<command>] -h     help about any command
-                `)
-		return nil
-	})
 	rootCmd.AddCommand(searchvar)
 	rootCmd.AddCommand(copyvar)
 	rootCmd.AddCommand(addvar)
@@ -61,6 +42,51 @@ Available Flags:
 	rootCmd.AddCommand(searchcmd)
 	rootCmd.AddCommand(removecmd)
 	rootCmd.AddCommand(copycmd)
+
+	//For usage generation
+	//TODO: find a better way to generate organized help message
+	//var varCmds []cobra.Command
+	//var cmdCmds []cobra.Command
+	//for _, c := range rootCmd.Commands() {
+	//	cmdName := c.Name()
+	//	if string(cmdName[len(cmdName)-1]) == "c" {
+	//		fmt.Println("cmd")
+	//		cmdCmds = append(cmdCmds, *c)
+	//	}
+	//	if string(cmdName[len(cmdName)-1]) == "v" {
+	//		fmt.Println("var")
+	//		varCmds = append(varCmds, *c)
+	//	}
+	//}
+
+	////for variable related Commands
+	//for _, c := range varCmds {
+	//	fmt.Println(c.Use, c.Short)
+	//}
+	//for _, c := range cmdCmds {
+	//	fmt.Println(c.Use, c.Short)
+	//}
+
+	rootCmd.SetUsageFunc(func(cmd *cobra.Command) error {
+		fmt.Println("Available Commands:")
+		fmt.Println("         help [<command>]        help about any command")
+		fmt.Println("")
+		fmt.Println("   [Variable related]")
+		fmt.Println("         av <variable> <value>   add <variable> <value> pair")
+		fmt.Println("         cv <variable>           copy <variable>'s <value> to system clipboard")
+		fmt.Println("         rv <variable>...        remove variables. If no <variable> provided, remove all variables")
+		fmt.Println("         sv [<search_query>]     search variable that contains <search_query>. If no query provided, print all variables")
+		fmt.Println("")
+		fmt.Println("   [Command related]")
+		fmt.Println("         ac [<key>]              add command with specified key. Key is optional")
+		fmt.Println("         cc <key>                copy a command replacing <variable> with its <value>")
+		fmt.Println("         rc <key>...             remove commands specified by keys")
+		fmt.Println("         sc [<search_query>]     search commands. If no query, get all commands")
+		fmt.Println("")
+		fmt.Println("Available Flags:")
+		fmt.Println("         kop [<command>] -h     help about any command")
+		return nil
+	})
 }
 
 func configInit() {
